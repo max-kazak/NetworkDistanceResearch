a8604d3258b68d74682cb38e304e0651e24899be
volterr <volterr@gmail.com>
2015-05-26 23:47:01 +0300

    knn with variable k

--- classification.R
+++ classification.R
@@ -1,11 +1,21 @@
 #function that marks one random vertex in each cluster
 #and reclassifies according to distance matrix
-classify <- function(D, marked) {
+classify <- function(D, marked, knn=1) {
     D.marked = D[,as.vector(marked$V)]
     
     graph.clust = sapply(rownames(D), function(v){
-        marked.v = colnames(D.marked)[which.min(D.marked[v,])]
-        marked[marked$V==marked.v,2]
+        min.ids = which(D.marked[v,] == sort(D.marked[v,])[1:knn])
+        min.dist = D.marked[v, min.ids]
+        min.names = names(min.ids)
+        min.class = sapply(min.names, function(min.name) marked[marked$V==min.name,2])
+        
+        class = as.numeric(names(which(table(min.class)==max(table(min.class)))))
+        if(length(class)>1) {
+            df.min.class = data.frame(class = min.class, dist = min.dist)
+            df.class.avgdist = aggregate(df.min.class[df.min.class$class %in% class,], by=class, mean)
+            class = df.class.avgdist[which.min(df.class.avgdist$dist),]$class
+        }
+        class
     })
     graph.clust
 }
@@ -18,7 +28,7 @@ classify.multiple <- function(graph, marked.mult, distance, alpha) {
     for(i in seq(length(marked.mult))) {
         #cat("=")
         class <- c(class,classify(D, marked.mult[[i]]))
-        dim(class) <- c(77,i)
+        dim(class) <- c(dim(D)[1],i)
     }
     rownames(class) <- V(graph)$name
     
@@ -28,21 +38,18 @@ classify.multiple <- function(graph, marked.mult, distance, alpha) {
 classify.multiple.voted <- function(graph, marked.mult, distance, alpha) {
     class <- classify.multiple(graph, marked.mult, distance, alpha)
     #voting across all classifications
+    
     class.vote <- apply(class,1,function(x) as.numeric(names(which.max(table(x)))))
     class.vote
 }
 
-select.marked.multiple <- function(clusters, num=100) {
-    l.marked = list()
-    for(i in seq(num)) {
-        marked = sapply(1:max(clusters$clust), function(i){
-            sample(clusters[clusters$clust==i,1], 1)
-        })
-        marked = levels(marked)[marked]
+select.marked.multiple <- function(clusters, n=100, k=1) {
+    lapply(1:n, function(i) {
+        marked = as.vector(sapply(1:max(clusters$clust), function(i){
+            sample(clusters[clusters$clust==i,1], k)
+        }))
         df.marked = clusters[clusters$V %in% marked,]
-        l.marked[[i]] = df.marked
-    }
-    l.marked
+    })
 }
 
 
